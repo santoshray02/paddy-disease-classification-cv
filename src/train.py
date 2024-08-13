911092039e82c4eb045c5ab7f6416629393ca105
Santosh Ray <santosh.ray@deepcognition.ai>
2024-08-13 12:25:46 +0530

    feat: use CPU device for model training

--- src/train.py
+++ src/train.py
@@ -26,7 +26,8 @@ def train(data_dir, model_name, batch_size=32, output_dir='./output', num_epochs
         results = model.train(data=train_loader, epochs=num_epochs, imgsz=640, batch=batch_size, save_dir=output_dir)
     else:
         model = get_model(model_name, num_classes=num_classes)  # No need to add 1 for background class
-        device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+        # device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+        device = torch.device('cpu')
         print(device)
         model.to(device)
         
